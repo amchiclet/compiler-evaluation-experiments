ac98e09c41adf9f69606df8a176f8dbcd39c5514
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2019-11-06 15:30:50 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+from enum import Enum, auto
+from random import choice
+
+class AutoName(Enum):
+  def _generate_next_value_(name, start, count, last_values):
+    return name
+
+class Relation(AutoName):
+  EQUAL = auto()
+  LESS = auto()
+  GREATER = auto()
+  INDIRECT = auto()
+  INDEPENDENT = auto()
+
+class AccessRelation:
+  def __init__(self, ref_access, other_access, relation):
+    self.ref_access = ref_access
+    self.other_access = other_access
+    self.relations = relations
+    
+class ArrayAccessExpr:
+  def __init__(self, var, access):
+    self.var = var
+    self.access = access
+  def __str__(self):
+    return f'{self.var}[{self.access}]'
+
+class BinaryExpr:
+  def __init__(self, op, lhs, rhs):
+    self.op = op
+    self.lhs = lhs
+    self.rhs = rhs
+  def __str__(self):
+    return f'({self.lhs} {self.op} {self.rhs})'
+
+class AssignStmt:
+  def __init__(self, lhs, rhs):
+    self.lhs = lhs
+    self.rhs = rhs
+  def __str__(self):
+    return f'{self.lhs} = {self.rhs}'
+
+class Var:
+  def __init__(self, name, dimensions):
+    self.name = name
+    self.dimensions = dimensions
+  def __str__(self):
+    return f'{self.name}'
+
+def generate_var():
+  var_name = choice(['A', 'B'])
+  return Var(var_name, 2)
+
+def generate_array_access_expr():
+  var = generate_var()
+  return ArrayAccessExpr(var, None)
+
+def generate_binary_expr():
+  op = choice(['+', '-', '*'])
+  lhs = generate_array_access_expr()
+  rhs = generate_array_access_expr()
+  return BinaryExpr(op, lhs, rhs)
+
+def generate_assignment():
+  lhs = generate_array_access_expr()
+  rhs = generate_binary_expr()
+  return AssignStmt(lhs, rhs)
+
+def generate_access_pattern(ast_template):
+  return ast_template
+
+def emit_c(ast):
+  print(ast)
+
+def generate_program():
+  ast_template = generate_assignment()
+  ast = generate_access_pattern(ast_template)
+  emit_c(ast)
+
+generate_program()
