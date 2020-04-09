a59df42791f95733bbdc07135de57658bb3164b1
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2020-04-08 20:09:38 -0500

    New version of dependence analysis is done

---
+++ system/loop_analyzer2.py
@@ -0,0 +1,184 @@
+from abstract_ast import get_accesses
+from random import shuffle, choice
+from z3_helper2 import DepRange, find_minimum_valid_index, find_min_max_range
+
+# given A[i][j],
+# return loop_var -> (array, dimension) -> [index expression]
+def group_indices(accesses):
+    outer_map = {}
+    for access in accesses:
+        array = access.var
+        for dimension, index in enumerate(access.indices):
+            loop_var = index.var
+            if not loop_var in outer_map:
+                outer_map[loop_var] = {}
+            inner_map = outer_map[loop_var]
+            inner_key = (array, dimension)
+            if not inner_key in inner_map:
+                inner_map[inner_key] = []
+            inner_map[inner_key].append(index)
+    return outer_map
+
+class LoopAnalysis:
+    def __init__(self, loop_var_analysis, array_analysis, dep_map):
+        self.loop_var_analysis = loop_var_analysis
+        self.array_analysis = array_analysis
+        self.dep_map = dep_map
+    def debug(self):
+        lines = [
+            self.loop_var_analysis.debug(),
+            self.array_analysis.debug()
+        ]
+        lines.append(f'Loop range analysis')
+        for (loop_var, array, dimension), dep_range_map in self.dep_map.items():
+            lines.append(f'{array}[{dimension}] (loop var {loop_var})')
+            lines.append(dep_range_map.pprint())
+        return '\n'.join(lines)
+
+def hashed(index1, index2):
+    return (index1.var, index1.coeff, index1.offset,
+            index2.var, index2.coeff, index2.offset)
+
+def has_overlap(range1, range2):
+    return range1[1] > range2[0] and range1[0] < range2[1]
+
+class DepRangeMap:
+    def __init__(self):
+        # maps hashed(index1, index2) to a DepRange
+        self.the_map = {}
+    def set_dep_range(self, index1, index2, dep_range):
+        hashed_index = hashed(index1, index2)
+        self.the_map[hashed_index] = dep_range
+    def pprint(self):
+        lines = []
+        for dep_range in self.the_map.values():
+            lines.append(dep_range.pprint())
+        return '\n'.join(lines)
+
+    def check_overlap(self, loop_var_range, i1, i2, attr):
+        i1_i2 = hashed(i1, i2)
+        if not i1_i2 in self.the_map:
+            return False
+        dep_range = attr(self.the_map[i1_i2])
+        if not dep_range:
+            return False
+        return has_overlap(loop_var_range, dep_range)
+
+    def is_loop_carried(self, loop_var_range, i1, i2):
+        return self.check_overlap(loop_var_range, i1, i2,
+                                  lambda x: x.loop_carried)
+    def is_loop_independent(self, loop_var_range, i1, i2):
+        return self.check_overlap(loop_var_range, i1, i2,
+                                  lambda x: x.loop_independent)
+
+class LoopVarAnalysis:
+    def __init__(self):
+        self.analysis = {}
+    def set_range(self, loop_var, min_val, max_val):
+        self.analysis[loop_var] = (min_val, max_val)
+    def min_val(self, loop_var):
+        return self.analysis[loop_var][0]
+    def max_val(self, loop_var):
+        return self.analysis[loop_var][1]
+    def debug(self):
+        lines = []
+        lines.append('Loop var ranges:')
+        for loop_var, (min_val, max_val) in self.analysis.items():
+            lines.append(f'  {loop_var} = [{min_val}..{max_val}]')
+        return '\n'.join(lines)
+
+class ArrayAnalysis:
+    def __init__(self):
+        self.arrays = {}
+    def set_range(self, array, dimension, min_index, max_index):
+        self.arrays[(array, dimension)] = (min_index, max_index)
+    def min_index(self, array, dimension):
+        return self.arrays[(array, dimension)][0]
+    def max_index(self, array, dimension):
+        return self.arrays[(array, dimension)][1]
+    def debug(self):
+        lines = []
+        lines.append('Array index ranges:')
+        for (array, dimension), (min_val, max_val) in self.arrays.items():
+            lines.append(f'  |{array}[{dimension}]| => [{min_val}..{max_val}]')
+        return '\n'.join(lines)
+
+from math import factorial
+def analyze_loop(loop):
+    loop_var_analysis = LoopVarAnalysis()
+    grouped_indices = group_indices(get_accesses(loop))
+
+    # maps (array, dimension) to another map which
+    # maps (index1, index2) to a DepRange
+    dep_map = {}
+
+    # find the range of possible loop var
+    for loop_var in loop.loop_vars:
+        loop_var_cut = set()
+        failed_outer = True
+
+        min_array_index = 0
+        for indices in grouped_indices[loop_var].values():
+            for index in indices:
+                min_array_index = find_minimum_valid_index(min_array_index, index)
+
+        min_val = min_array_index
+        max_val = 500
+        failed_inner = False
+        for (array, dimension), indices in grouped_indices[loop_var].items():
+            # print(f'Analyzing array {array}[{dimension}]')
+            dep_ranges = DepRangeMap()
+            for index1 in indices:
+                for index2 in indices:
+                    if index1.is_syntactically_equal(index2):
+                        continue
+                    dep_range = DepRange(min_val, max_val, index1, index2)
+                    dep_ranges.set_dep_range(index1, index2, dep_range)
+                    if dep_range.loop_carried:
+                        loop_var_cut.update(dep_range.loop_carried)
+                    if dep_range.loop_independent:
+                        loop_var_cut.update(dep_range.loop_independent)
+            dep_map[(loop_var, array, dimension)] = dep_ranges
+
+        # print(f'loop var {loop_var} has cuts {sorted(list(loop_var_cut))}')
+        # find all non-trivial cuts (cuts with length greater than 1)
+        sorted_cuts = sorted(list(loop_var_cut))
+        non_trivial_ranges = []
+        for i in range(len(sorted_cuts) - 1):
+            if sorted_cuts[i+1] - sorted_cuts[i] > 1:
+                non_trivial_ranges.append(tuple(sorted_cuts[i:i+2]))
+        loop_var_range = choice(non_trivial_ranges)
+        # print(f'loop var {loop_var} has range {loop_var_range}')
+        loop_var_analysis.set_range(loop_var, *loop_var_range)
+
+    # figure out array sizes
+    array_analysis = ArrayAnalysis()
+    for loop_var, indices_map in grouped_indices.items():
+        for (array, dimension), indices in indices_map.items():
+            min_loop_var_val = loop_var_analysis.min_val(loop_var)
+            max_loop_var_val = loop_var_analysis.max_val(loop_var)
+            min_range = 500
+            max_range = 0
+            for index in indices:
+                new_min_range, new_max_range = find_min_max_range(
+                    min_loop_var_val,
+                    max_loop_var_val,
+                    index)
+                if new_min_range < min_range:
+                    min_range = new_min_range
+                if new_max_range > max_range:
+                    max_range = new_max_range
+            array_analysis.set_range(array, dimension, min_range, max_range)
+    loop_analysis = LoopAnalysis(loop_var_analysis, array_analysis, dep_map)
+    return loop_analysis
+
+max_tries = 1
+def analyze_loops(program):
+    all_analysis = {}
+    for loop in program.loops:
+        for _ in range(max_tries):
+            analysis = analyze_loop(loop)
+            if analysis:
+                all_analysis[loop.node_id] = analysis
+                break
+    return all_analysis
---
+++ system/z3_helper2.py
@@ -0,0 +1,157 @@
+from z3 import Solver, Ints, unsat, Optimize, sat, Or
+from random import choice
+
+# assuming i1 < i2
+def find_min_max(constraints, i):
+    min_optimize = Optimize()
+    min_optimize.assert_exprs(*constraints)
+    min_optimize.minimize(i)
+    assert(sat == min_optimize.check())
+
+    max_optimize = Optimize()
+    max_optimize.assert_exprs(*constraints)
+    max_optimize.maximize(i)
+    assert(sat == max_optimize.check())
+    return [
+        min_optimize.model().eval(i).as_long(),
+        max_optimize.model().eval(i).as_long() + 1,
+    ]
+
+# TODO: move the z3 stuff into functions and move this class to somewhere else
+class DepRange:
+    def __init__(self, min_i, max_i, index1, index2):
+        # finds the range of i such that there is a dependence from index1 to index2
+        # returns a dep range instance
+        self.index1 = index1
+        self.index2 = index2
+        self.min_i = min_i
+        self.max_i = max_i
+
+        if index1.var and index2.var:
+            assert(index1.var == index2.var)
+
+        # i is the index variable and s* are the subscripts
+        # expect s1 < s2
+        i1, i2, s1, s2 = Ints('i1 i2 s1 s2')
+
+        i1_min_constraint = min_i <= i1
+        i1_max_constraint = i1 < max_i
+        i2_min_constraint = min_i <= i2
+        i2_max_constraint = i2 < max_i
+
+        s1 = i1 * index1.coeff + index1.offset
+        s2 = i2 * index2.coeff + index2.offset
+        s1_min_constraint = s1 >= 0
+        s2_min_constraint = s2 >= 0
+
+        # dependence constraint
+        same_mem = s1 == s2
+
+        constraints = [
+            i1_min_constraint,
+            i1_max_constraint,
+            i2_min_constraint,
+            i2_max_constraint,
+            s1_min_constraint,
+            s2_min_constraint,
+            same_mem
+        ]
+        solver = Solver()
+        solver.add(constraints)
+
+        # check for loop independent dependence
+        status = solver.check(i1 == i2)
+        if status == sat:
+            # range where i1 == i2
+            new_constraints = constraints + [i1 == i2]
+            min_max_i1 = find_min_max(new_constraints, i1)
+            self.loop_independent = min_max_i1
+        else:
+            assert(status == unsat)
+            self.loop_independent = None
+
+        # check for loop carried from index1 to index2
+        # it's only loop carried when it's not loop independent
+        if self.loop_independent:
+            # solver.add(i1 < self.loop_independent[0])
+            # solver.add(i2 < self.loop_independent[0])
+            solver.add(Or(i1 < self.loop_independent[0],
+                          i1 >= self.loop_independent[1]))
+            solver.add(Or(i2 < self.loop_independent[0],
+                          i2 >= self.loop_independent[1]))
+
+        status = solver.check(i1 < i2)
+
+        if status == sat:
+            # print('finding loop-carried')
+            # range where i1 < i2
+            new_constraints = constraints + [i1 < i2]
+            min_max_i1 = find_min_max(new_constraints, i1)
+            min_max_i2 = find_min_max(new_constraints, i2)
+            # print(min_max_i1)
+            # print(min_max_i2)
+            # print(solver.check(i1 == min_max_i1[0]))
+            # print(solver.model().eval(i1),
+            #       solver.model().eval(i2),
+            #       solver.model().eval(s1),
+            #       solver.model().eval(s2))
+            self.loop_carried = [min(min_max_i1[0], min_max_i1[0]),
+                                 max(min_max_i2[1], min_max_i2[1])]
+            # print(self.loop_carried)
+        else:
+            assert(status == unsat)
+            self.loop_carried = None
+
+    def is_independent(self):
+        return not self.loop_carried and not self.loop_independent
+
+    def pprint(self):
+        return (f'\t{self.index1.pprint()} -> {self.index2.pprint()}\n'
+                f'\tLoop-carried({self.loop_carried})\n'
+                f'\tLoop-independent({self.loop_independent})')
+
+def find_minimum_valid_index(min_val, use):
+    i, = Ints('i')
+    solver = Solver()
+
+    i_abs_min_constraint = min_val <= i
+    i_min_constraint = i * use.coeff + use.offset >= 0
+    
+    # find min
+    find_min = Optimize()
+    find_min.assert_exprs(i_abs_min_constraint,
+                          i_min_constraint)
+    find_min.minimize(i)
+    assert(sat == find_min.check())
+    return find_min.model().eval(i).as_long()
+
+def find_min_max_range(min_val, max_val, index):
+    # give a current range of i
+    # give an index that uses i
+    # find the min and max int that covers all the index
+
+    i, s = Ints('i s')
+    solver = Solver()
+
+    i_min_constraint = min_val <= i
+    i_max_constraint = i < max_val
+    s_constraint = s == i * index.coeff + index.offset
+
+    # find min
+    find_min = Optimize()
+    find_min.assert_exprs(i_min_constraint,
+                          i_max_constraint,
+                          s_constraint)
+    find_min.minimize(s)
+    assert(sat == find_min.check())
+    min_val = find_min.model().eval(s)
+
+    # find max
+    find_max = Optimize()
+    find_max.assert_exprs(i_min_constraint,
+                          i_max_constraint,
+                          s_constraint)
+    find_max.maximize(s)
+    assert(sat == find_max.check())
+    max_val = find_max.model().eval(s)
+    return [min_val.as_long(), max_val.as_long()]
