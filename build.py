3b6d58c1b8ed32ff71da26eb493b3fe02a7796ed
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2020-08-30 00:06:41 -0500

    Build files are kept in a separate directory

--- build.py
+++ build.py
@@ -1,7 +1,13 @@
 from compilers import compilers, base_command, novec_flags, nopredict_flags
 import inspect
+from pathlib import Path
 
 class PathBuilder:
+    build_dir = 'build'
+    results_dir = 'results'
+    Path(build_dir).mkdir(parents=True, exist_ok=True)
+    Path(results_dir).mkdir(parents=True, exist_ok=True)
+
     def __init__(self, compiler=None, mode=None, pattern=None, program=None, mutation=None):
         self.compiler = compiler
         self.mode = mode
@@ -24,6 +30,10 @@ class PathBuilder:
                                [ self.source_part(pattern, program, mutation),
                                  self.compiler_part(compiler, mode)
                                ]))
+    def build_prefix(self):
+        return f'{self.build_dir}/{self.prefix()}'
+    def results_prefix(self):
+        return f'{self.results_dir}/{self.prefix()}'
     def compiler_part(self, compiler=None, mode=None):
         return '.'.join(filter(lambda i: len(i) > 0,
                                [ self.compiler_str(compiler),
@@ -51,39 +61,39 @@ class PathBuilder:
         return pb
 
     def exe_path(self):
-        return f'{self.prefix()}.exe'
+        return f'{self.build_prefix()}.exe'
 
     def main_path(self):
         return f'main.c'
     def main_obj_path(self):
-        return f'{self.prefix()}.main.o'
+        return f'{self.build_prefix()}.main.o'
 
     def core_path(self):
         return f'{self.source_part()}.c'
 
     def core_obj_path(self):
-        return f'{self.prefix()}.o'
+        return f'{self.build_prefix()}.o'
 
     def wrapper_path(self):
         return f'{self.source_part()}.wrapper.c'
 
     def wrapper_obj_path(self):
-        return f'{self.prefix()}.wrapper.o'
+        return f'{self.build_prefix()}.wrapper.o'
 
     def assembly_path(self):
-        return f'{self.prefix()}.s'
+        return f'{self.build_prefix()}.s'
 
     def runtimes_ns_path(self, mutation=None):
-        return f'{self.prefix(mutation=mutation)}.nanosec_runtimes'
+        return f'{self.results_prefix()}.nanosec_runtimes'
 
     def ns_per_iteration_path(self):
-        return f'{self.prefix()}.nanosec_per_iteration'
+        return f'{self.results_prefix()}.nanosec_per_iteration'
 
     def vector_rate_path(self):
-        return f'{self.prefix()}.vector_rate'
+        return f'{self.results_prefix()}.vector_rate'
 
-    def checksum_path(self, mutation=None):
-        return f'{self.prefix(mutation=mutation)}.checksum'
+    def checksum_path(self):
+        return f'{self.results_prefix()}.checksum'
 
 class CommandBuilder:
     def link_objects(self, compiler, mode, objects, libs, output):
