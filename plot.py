8df050bfa5efa0ae8e7a8ae19d3d7f6731019e75
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2020-09-03 18:17:05 -0500

    Saveplot and clear plot support for multiple figures

--- plot.py
+++ plot.py
@@ -12,8 +12,14 @@ def display_plot(title=None):
         plt.title(title)
     plt.show()
 
-def save_plot(path):
+def save_plot(path, title=None):
+    if title is not None:
+        plt.title(title)
     plt.savefig(path)
+
+def clear_plot():
+    plt.close()
+    # plt.clf()
 
 def add_dict_array(da, stat, b=100, min_val=None, max_val=None):
     for k, vs in da.items():
@@ -24,7 +30,7 @@ def add_dict_array(da, stat, b=100, min_val=None, max_val=None):
             data.append(stat(samples))
         add_plot(data, k, min_val, max_val)
 
-def add_dddl(pattern_maps, n_patterns, n_instances, stat, b=100, min_val=None, max_val=None):
+def add_dddl(pattern_maps, stat, n_patterns=None, n_instances=None, b=100, min_val=None, max_val=None):
     for compiler, pattern_map in pattern_maps.items():
         patterns = []
         for pattern, instance_map in pattern_map.items():
@@ -34,14 +40,13 @@ def add_dddl(pattern_maps, n_patterns, n_instances, stat, b=100, min_val=None, m
             patterns.append((pattern, instances))
 
         data = []
-        print(f'Plotting: {compiler}')
         for _ in tqdm(range(b)):
-            print('      new sample')
             samples = []
-            for pattern, instances in choices(patterns, k=n_patterns):
-                for instance, runtimes in choices(instances, k=n_instances):
+            k_patterns = len(patterns) if n_patterns is None else n_patterns
+            for pattern, instances in choices(patterns, k=k_patterns):
+                k_instances = len(instances) if n_instances is None else n_instances
+                for instance, runtimes in choices(instances, k=k_instances):
                     samples += runtimes
-                    print(runtimes)
             data.append(stat(samples))
         add_plot(data, compiler, min_val, max_val)
 
