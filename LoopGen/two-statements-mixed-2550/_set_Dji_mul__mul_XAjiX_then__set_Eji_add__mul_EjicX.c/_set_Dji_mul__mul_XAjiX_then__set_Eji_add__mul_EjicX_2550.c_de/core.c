7100ecfa3bf354b8047093a37036ac6fb480a952
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2021-03-26 10:18:18 -0700

    Generated two-statements-mixed

---
+++ LoopGen/two-statements-mixed-2550/_set_Dji_mul__mul_XAjiX_then__set_Eji_add__mul_EjicX.c/_set_Dji_mul__mul_XAjiX_then__set_Eji_add__mul_EjicX_2550.c_de/core.c
@@ -0,0 +1,21 @@
+#include <x86intrin.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <string.h>
+
+int core(double A[restrict 2550][2550], double B[restrict 1][1], double C[restrict 1][1], double D[restrict 2550][2550], double E[restrict 2550][2550], double a, double b, double c) {
+  for (int i = 0; i <= 2549; i+=1) {
+    for (int j = 0; j <= 2549; j+=1) {
+        D[j][i] = (1.5 * A[j][i]) * 1.5;
+        E[j][i] = E[j][i] * c + 1.5;
+    }
+  }
+  return 0;
+}
+
