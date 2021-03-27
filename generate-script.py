878be487043549bac2e81b3d470b7fecfca48ea5
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2021-03-27 10:03:59 -0700

    Committed the correct code generation script

--- generate-script.py
+++ generate-script.py
@@ -1,88 +1,131 @@
-from pathlib import Path
-from c_generator import CGenerator
-from shutil import copytree
+from skeleton import \
+    parse_str as parse_skeleton, \
+    parse_stmt_str as parse_statement, \
+    parse_seq_str as parse_seq, \
+    parse_expr_str as parse_expression
 
-def conf_contents(codelet):
-    return (f'<?xml version="1.0" ?>\n'
-            f'<codelet>\n'
-            f'  <language value="C"/>\n'
-            f'  <label name="{codelet}"/>\n'
-            f'  <function name="core"/>\n'
-            f'  <binary name="wrapper"/>\n'
-            f'</codelet>\n')
+from pattern import generate_patterns_from_skeleton
+from skeleton_ast import Var, Op
+from random import choice, seed
+from instance import create_instance_with_fixed_size
+from codelet_generator import generate_codelet_full, name
+from populator import PopulateParameters, populate_name, populate_stmt, populate_expr, populate_op
 
-def meta_contents(batch, code, codelet):
-    return (f'application name=LoopGen\n'
-            f'batch name={batch}\n'
-            f'code name={code}\n'
-            f'codelet name={codelet}\n')
+seed(12345)
+skeleton_code = """
+declare A[][];
+declare B[][];
+declare C[][];
+declare D[][];
+declare E[][];
+declare a;
+declare b;
+declare c;
 
-def data_contents(n_iterations):
-    return f'{n_iterations} 0\n'
+for [i, j] {
+  $_:stmt$
+  $_:stmt$
+}
+"""
 
-def codelet_dir(application, batch, code, codelet, base='.'):
-    return f'{base}/{application}/{batch}/{code}/{codelet}'
+scalar_names = ['a', 'b', 'c']
+array_names = ['A', 'B', 'C', 'D', 'E']
+index_names = ['i', 'j']
+op_names = ['+', '*']
 
-def meta_file(codelet_dir):
-    return f'{codelet_dir}/codelet.meta'
+def create_exploration_factory():
+    stmt = ['#_:lhs# = #_:rhs# @_:op@ #_:rhs# @_:op@ #_:rhs#;']
+    lhs = ['`_:X_lhs`[`i1:I`][`i2:I`]']
+    rhs = ['`_:X_rhs`[`i1:I`][`i2:I`]', '`_:S_rhs`', '1.5']
 
-def conf_file(codelet_dir):
-    return f'{codelet_dir}/codelet.conf'
+    stmts = PopulateParameters()
+    stmts.add('stmt', [parse_statement(code) for code in stmt])
 
-def data_file(codelet_dir):
-    return f'{codelet_dir}/codelet.data'
+    exprs = PopulateParameters()
+    exprs.add('lhs', [parse_expression(code) for code in lhs])
+    exprs.add('rhs', [parse_expression(code) for code in rhs])
 
-def prepare_output_dir(dst_dir):
-    copytree('c-template', dst_dir, dirs_exist_ok=True)
+    ops = PopulateParameters()
+    ops.add('op', list(map(Op, op_names)))
 
-def name_many(nodes, delimiter=''):
-    return delimiter.join([name(node) for node in nodes])
+    names = PopulateParameters()
+    names.add('X_lhs', list(map(Var, array_names)))
+    names.add('X_rhs', list(map(Var, array_names)))
+    names.add('S_rhs', list(map(Var, scalar_names)))
+    names.add('I', list(map(Var, index_names)), is_finite=True)
 
-def name(node):
-    from pattern_ast import Hex, Assignment, AbstractLoop, Literal, Access, Program, Op
-    ty = type(node)
-    if ty == Literal:
-        return 'X'
-    if ty == Hex:
-        return node.str_val
-    if ty == Assignment:
-        return f'_set_{name(node.lhs)}{name(node.rhs)}'
-    if ty == Access:
-        return f'{node.var}{name_many(node.indices)}'
-    if ty == AbstractLoop:
-        return '_then_'.join([name(stmt) for stmt in node.body])
-    if ty == Op:
-        op_map = {
-            '+': '_add_',
-            '*': '_mul_'
-        }
-        return f'{op_map[node.op]}{name_many(node.args)}'
-    if ty == Program:
-        assert(len(node.body) == 1)
-        return name(node.body[0])
-    print(ty)
+    def generate(code):
+        code = populate_stmt(code, stmts.populate)
+        code = populate_expr(code, exprs.populate)
+        code = populate_op(code, ops.populate)
+        code = populate_name(code, names.populate)
+        return code
+    return generate
 
-def generate_codelet_full(application, batch, code, codelet, n_iterations, instance):
-    dst_dir = codelet_dir(application, batch, code, codelet)
-    prepare_output_dir(dst_dir)
+def create_targeted_factory():
+    stmt = [
+        '#_:shared# = #_:rhs# @_:op@ #_:rhs# @_:op@ #_:rhs#;',
+        '#_:lhs# = #_:shared# @_:op@ #_:rhs# @_:op@ #_:rhs#;',
+        '#_:lhs# = #_:rhs# @_:op@ #_:shared# @_:op@ #_:rhs#;',
+        '#_:lhs# = #_:rhs# @_:op@ #_:rhs# @_:op@ #_:shared#;',
+    ]
+    shared = ['`x:X_shared`[`i1:I`][`i2:I`]']
+    lhs = ['`_:X_lhs`[`i1:I`][`i2:I`]']
+    rhs = ['`_:X_rhs`[`i1:I`][`i2:I`]', '`_:S_rhs`', '1.5']
 
-    Path(meta_file(dst_dir)).write_text(meta_contents(batch, code, codelet))
-    Path(conf_file(dst_dir)).write_text(conf_contents(codelet))
-    Path(data_file(dst_dir)).write_text(data_contents(n_iterations))
+    stmts = PopulateParameters()
+    stmts.add('stmt', [parse_statement(code) for code in stmt])
 
-    cgen = CGenerator(instance)
-    print(cgen.core())
-    wrapper_path = f'{dst_dir}/wrapper.c'
-    cgen.write_kernel_wrapper(wrapper_path)
-    core_path = f'{dst_dir}/core.c'
-    cgen.write_core(core_path)
+    exprs = PopulateParameters()
+    exprs.add('shared', [parse_expression(code) for code in shared])
+    exprs.add('lhs', [parse_expression(code) for code in lhs])
+    exprs.add('rhs', [parse_expression(code) for code in rhs])
 
-def generate_codelet(batch, instance):
-    # C code generation
-    application = 'LoopGen'
+    ops = PopulateParameters()
+    ops.add('op', list(map(Op, op_names)))
+
+    names = PopulateParameters()
+    names.add('X_shared', list(map(Var, array_names)))
+    names.add('X_lhs', list(map(Var, array_names)))
+    names.add('X_rhs', list(map(Var, array_names)))
+    names.add('S_rhs', list(map(Var, scalar_names)))
+    names.add('I', list(map(Var, index_names)), is_finite=True)
+
+    def generate(code):
+        code = populate_stmt(code, stmts.populate)
+        code = populate_expr(code, exprs.populate)
+        code = populate_op(code, ops.populate)
+        code = populate_name(code, names.populate)
+        return code
+    return generate
+
+skeleton = parse_skeleton(skeleton_code)
+
+generate_exploratory_code = create_exploration_factory()
+generate_targeted_code = create_targeted_factory()
+
+patterns = []
+hashes = set()
+
+patterns += generate_patterns_from_skeleton(
+    generate_targeted_code,
+    skeleton,
+    n_wanted = 50,
+    existing_hashes = hashes)
+patterns += generate_patterns_from_skeleton(
+    generate_exploratory_code,
+    skeleton,
+    n_wanted = 250,
+    existing_hashes = hashes)
+
+application = 'LoopGen'
+size = 2550
+n_iterations = 10
+for pattern in patterns:
+    instance = create_instance_with_fixed_size(pattern, index_names, size)
+    print(instance.pattern.pprint())
     code_prefix = f'{name(instance.pattern)}'
     code = f'{code_prefix}.c'
-    codelet = f'{code_prefix}.c_de'
-    n_iterations = 10
+    codelet = f'{code_prefix}_{size}.c_de'
 
-    generate_codelet_full(application, batch, code, codelet, n_iterations, instance)
+    generate_codelet_full(application, 'two-statements-mixed-2550', code, codelet, n_iterations, instance)
