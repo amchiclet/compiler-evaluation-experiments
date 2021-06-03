c786ef680c394421af3a27e98ceed3eb6291280f
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2021-06-03 08:37:32 -0700

    Added code generation script for s254

---
+++ code-generation-scripts/s254.py
@@ -0,0 +1,54 @@
+from pattern import parse_str as parse_pattern
+from instance import create_instance
+from constant_assignment import VariableMap
+from type_assignment import TypeAssignment
+from codegen.c_generator import generate_code
+from codelet_generator import generate_codelet_full, name
+
+code = """
+declare A[n+1];
+declare B[n+1];
+declare x;
+declare n;
+
+for [(i, >=0, <=n)] {
+  A[i] = (B[i] + x) * 0.5;
+  x = B[i];
+}
+"""
+
+pattern = parse_pattern(code)
+
+artificial_size_limit = 999999
+var_map = VariableMap()
+var_map.set_value('n', artificial_size_limit)
+var_map.set_value('m', artificial_size_limit)
+
+# Specify variable types
+types = TypeAssignment()
+types.set('A', 'double')
+types.set('B', 'double')
+types.set('x', 'double')
+types.set('n', 'int')
+
+instance = create_instance(pattern, var_map, types=types)
+print(instance.pprint())
+
+init_value_map = {
+    'A': 'drand(0.0, 1.0)',
+    'B': 'drand(0.0, 1.0)',
+    'x': 'drand(0.0, 1.0)',
+    'n': 'n_elements',
+}
+
+
+application = 'LoopGen'
+batch = f'lore'
+code_prefix = f'S254'
+code = f'{code_prefix}.c'
+codelet = f'{code_prefix}.c_de'
+n_iterations = 10
+n_elements = artificial_size_limit
+
+generate_codelet_full('LoopGen', batch, code, codelet,
+                      n_iterations, n_elements, instance, init_value_map)
