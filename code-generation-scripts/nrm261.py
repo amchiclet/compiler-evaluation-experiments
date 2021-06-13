eb718bf0341a1e10e47a8fef6bb69226bb35d5d1
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2021-06-13 13:13:47 -0700

    Updated generation script for nrm261

--- code-generation-scripts/nrm261.py
+++ code-generation-scripts/nrm261.py
@@ -1,15 +1,19 @@
-from pattern import parse_str as parse_pattern
+from pattern import parse_str as parse_pattern, generate_patterns_from_skeleton
 from instance import create_instance
 from constant_assignment import VariableMap
 from type_assignment import TypeAssignment
 from codegen.c_generator import generate_code
 from codelet_generator import generate_codelet_full, name
 from skeleton import parse_str as parse_skeleton, \
-    parse_stmt_str as parse_stmt
+    parse_stmt_str as parse_stmt, \
+    generate_from_skeleton
 
 from populator import PopulateParameters, populate_stmt
-from random import randint
+from random import randint, seed
 
+seed(0)
+
+grid_point_size = 64 / 2 * 2 + 1 + 1
 main = """
 declare grid_points[3];
 declare add;
@@ -53,50 +57,64 @@ class AtMostNMax:
         self.current += 1
         return c
 
-var_map = VariableMap()
-grid_point_size = 64 / 2 * 2 + 1 + 1
-var_map.set_value('grid_points[0]', grid_point_size)
-var_map.set_value('grid_points[1]', grid_point_size)
-var_map.set_value('grid_points[2]', grid_point_size)
-var_map.set_value('m_end', randint(2, 16))
+def generate(skeleton):
+    depth_control = AtMostNMax(
+        1, # index 1 means to skip the loop
+        3, # we're deciding from the outer 3 loops which ones to skip
+        1  # we can skip at most 1
+    )
 
-n_outer_loops = 3
-max_skips = 1
-n_choices_for_each_loop = 2
-depth_control = AtMostNMax(
-    1, # index 1 means to skip the loop
-    3, # we're deciding from the outer 3 loops which ones to skip
-    1  # we can skip at most 1
-)
+    parameters = PopulateParameters()
+    parameters.add('_', [parse_stmt(code) for code in loop_i], choice_function=depth_control.choice)
+    skeleton = populate_stmt(skeleton, parameters.populate)
+
+    parameters = PopulateParameters()
+    parameters.add('_', [parse_stmt(code) for code in loop_j], choice_function=depth_control.choice)
+    skeleton = populate_stmt(skeleton, parameters.populate)
+
+    parameters = PopulateParameters()
+    parameters.add('_', [parse_stmt(code) for code in loop_k], choice_function=depth_control.choice)
+    skeleton = populate_stmt(skeleton, parameters.populate)
+
+    parameters = PopulateParameters()
+    parameters.add('_', [parse_stmt(loop_m)])
+    skeleton = populate_stmt(skeleton, parameters.populate)
+
+    pattern = parse_pattern(skeleton.pprint())
+
+    n_inner_iterations = randint(2, 16)
+    var_map = VariableMap()
+    var_map.set_value('grid_points[0]', grid_point_size)
+    var_map.set_value('grid_points[1]', grid_point_size)
+    var_map.set_value('grid_points[2]', grid_point_size)
+    var_map.set_value('m_end', n_inner_iterations)
+
+    # Specify variable types
+    types = TypeAssignment(default_types=['int'])
+    types.set('add', 'double')
+    types.set('rhs', 'double')
+    types.set('rms', 'double')
+
+    instance = create_instance(pattern, var_map, types=types, max_tries=100)
+    if instance is None:
+        return None
+
+    chosen_loops = [var
+                    for choice, var
+                    in zip(depth_control.choices, ['i', 'j', 'k'])
+                    if choice == 0]
+    name = f'nrm261_{"_".join(chosen_loops)}_m_{n_inner_iterations}'
+
+    return instance, name
 
 skeleton = parse_skeleton(main)
+instances = {}
 
-parameters = PopulateParameters()
-parameters.add('_', [parse_stmt(code) for code in loop_i], choice_function=depth_control.choice)
-skeleton = populate_stmt(skeleton, parameters.populate)
-
-parameters = PopulateParameters()
-parameters.add('_', [parse_stmt(code) for code in loop_j], choice_function=depth_control.choice)
-skeleton = populate_stmt(skeleton, parameters.populate)
-
-parameters = PopulateParameters()
-parameters.add('_', [parse_stmt(code) for code in loop_k], choice_function=depth_control.choice)
-skeleton = populate_stmt(skeleton, parameters.populate)
-
-parameters = PopulateParameters()
-parameters.add('_', [parse_stmt(loop_m)])
-skeleton = populate_stmt(skeleton, parameters.populate)
-
-pattern = parse_pattern(skeleton.pprint())
-
-# Specify variable types
-types = TypeAssignment(default_types=['int'])
-types.set('add', 'double')
-types.set('rhs', 'double')
-types.set('rms', 'double')
-
-instance = create_instance(pattern, var_map, types=types, max_tries=1)
-print(instance.pprint())
+generate_from_skeleton(
+    generate,
+    skeleton,
+    n_wanted = 15,
+    outputs=instances)
 
 init_value_map = {
     'grid_points': str(grid_point_size),
@@ -105,14 +123,14 @@ init_value_map = {
     'rms': 'drand(0.0, 1.0)',
 }
 
-name = 'nrm261'
-application = 'LoopGen'
-batch = f'lore'
-code_prefix = name
-code = f'{code_prefix}.c'
-codelet = f'{code_prefix}.c_de'
-n_iterations = 10
-n_elements = 0
+for name, instance in instances.items():
+    application = 'LoopGen'
+    batch = f'lore'
+    code_prefix = name
+    code = f'{code_prefix}.c'
+    codelet = f'{code_prefix}.c_de'
+    n_iterations = 10
+    n_elements = 0
 
-generate_codelet_full('LoopGen', batch, code, codelet,
-                      n_iterations, n_elements, instance, init_value_map)
+    generate_codelet_full('LoopGen', batch, code, codelet,
+                          n_iterations, n_elements, instance, init_value_map)
