affdb94d6bf35085c8a6a96922f86b88eb702071
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2022-05-01 15:17:33 -0700

    Added code to xls parser

---
+++ code_to_xls.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+from pathlib import Path
+
+batch = 'LoopGen/reduction-unique-arrays-dbl-scalars'
+
+names = []
+codes = []
+
+for path in sorted(Path(batch).glob('*/*/core.c')):
+    codelet = path.parts[-3]
+    names.append(codelet)
+    lines = []
+    with open(path) as f:
+        function_start = False
+        for line in f:
+            if 'core(' in line:
+                function_start = True
+                continue
+            if 'return 0' in line:
+                break
+            if not function_start:
+                continue
+            lines.append(line)
+    no_empty = []
+    for line in lines:
+        if len(line.strip()) > 0:
+            no_empty.append(line)
+    codes.append(''.join(no_empty))
+
+df = pd.DataFrame()
+df['name'] = names
+df['code'] = codes
+df.to_excel('output.xlsx')
