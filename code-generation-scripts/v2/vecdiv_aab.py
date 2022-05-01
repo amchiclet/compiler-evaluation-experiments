7e4b2c25454f9ec0b82806bf024a7b8b58d5fc05
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2022-05-01 14:42:08 -0700

    Added code generation script for vecdiv var size

---
+++ code-generation-scripts/v2/vecdiv_aab.py
@@ -0,0 +1,46 @@
+from api import Skeleton, Mapping, CodegenConfig
+from random import randint, seed
+from codelet_generator import codelet_dir, generate_codelet_files, generate_batch_summary, SourceInfo
+from pattern_ast import get_ordered_assignments, get_accesses
+
+application = 'LoopGen'
+batch = 'vecdiv-api-v2'
+code = f'a_ab'
+codelet = f'{code}_de'
+
+skeleton_code = """
+declare a[N];
+declare b[N];
+declare N;
+
+for [(i, >=0, <=N-1)] {
+  a[i] = a[i] + b[i];
+}
+"""
+
+program = Skeleton(skeleton_code)
+
+config = CodegenConfig()
+
+config.possible_values = {
+    'a[i]': 1,
+    'b[i]': 1,
+    'N': 1,
+}
+
+config.types = {
+    'a[i]': 'double',
+    'a[i]': 'double',
+    'N': 'int',
+}
+
+config.initial_values = {
+    'a[i]' : 'drand(0.01, 1.0)',
+    'b[i]' : 'drand(0.01, 1.0)',
+    'N': 'inputs[0]',
+}
+
+config.template_dir = 'codelet-template-int-inputs'
+config.output_dir = generate_codelet_files(application, batch, code, codelet, 10, [32000])
+config.array_as_ptr = True
+program.generate_code(config)
