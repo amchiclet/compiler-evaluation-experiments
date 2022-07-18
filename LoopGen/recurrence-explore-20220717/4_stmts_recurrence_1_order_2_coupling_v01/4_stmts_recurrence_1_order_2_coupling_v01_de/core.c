21c78a72915f226c4841d23df042e8ded93da8a9
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2022-07-17 21:40:44 -0700

    Add generated code for recurrences

---
+++ LoopGen/recurrence-explore-20220717/4_stmts_recurrence_1_order_2_coupling_v01/4_stmts_recurrence_1_order_2_coupling_v01_de/core.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <string.h>
+
+extern int n_arrs;
+extern int size;
+
+int core(double (*restrict A)[2097152], double (*restrict B)[2097152], double (*restrict C)[2097152], double (*restrict D)[2097152]) {
+
+  for (int i = 1; i <= (size - 1) / n_arrs; i += 1) {
+    (*D)[i] = 0 * (*D)[i - 1];
+    (*A)[i] = 2 * (*A)[i - 1] + 0 * (*B)[i - 1];
+    (*C)[i] = 2 * (*C)[i - 1] + 0;
+    (*B)[i] = 2 * (*B)[i - 1] + 2 * (*A)[i - 1];
+  }
+  return 0;
+}
---
+++ LoopGen/recurrence-explore-20220717/4_stmts_recurrence_2_order_3_coupling_v08/4_stmts_recurrence_2_order_3_coupling_v08_de/core.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <string.h>
+
+extern int n_arrs;
+extern int size;
+
+int core(double (*restrict A)[2097152], double (*restrict B)[2097152], double (*restrict C)[2097152], double (*restrict D)[2097152]) {
+
+  for (int i = 2; i <= (size - 1) / n_arrs; i += 1) {
+    (*D)[i] = 1 * (*D)[i - 1] + 0 * (*D)[i - 2];
+    (*C)[i] = (2 * (*C)[i - 1] + 1 * (*C)[i - 2]) + (1 * (*A)[i - 1] + 2 * (*A)[i - 2]);
+    (*B)[i] = (2 * (*B)[i - 1] + 0 * (*B)[i - 2]) + (0 * (*C)[i - 1] + 1 * (*C)[i - 2]);
+    (*A)[i] = (0 * (*A)[i - 1] + 2 * (*A)[i - 2]) + (2 * (*B)[i - 1] + 1 * (*B)[i - 2]);
+  }
+  return 0;
+}
