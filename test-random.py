9ffa922ef9304f1fa106b93957d53c8442b52833
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2022-07-17 21:41:04 -0700

    Add utility script to randomly test a codelet

---
+++ test-random.py
@@ -0,0 +1,26 @@
+import delegator
+from pathlib import Path
+import sys
+from random import choice
+
+assert(len(sys.argv) == 2)
+
+def make_and_run(codelet):
+    c = delegator.run('make', cwd=codelet)
+    if c.return_code != 0:
+        print(f'Failed to make: {c.err}')
+        return
+    print(c.out)
+    c = delegator.run('./wrapper', cwd=codelet)
+    if c.return_code != 0:
+        print(f'Failed to run: {c.err}')
+        return
+    print(c.out)
+
+codelets = [path.parent for path in Path(sys.argv[1]).rglob('core.c')]
+codelet = choice(codelets)
+print(codelet)
+make_and_run(codelet)
+c = delegator.run('make clean', cwd=codelet)
+print(c.out)
+print(c.err)
