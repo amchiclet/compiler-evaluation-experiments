aabd8e5b034bd0c35e1a5879c5636613af951803
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2022-07-30 21:12:11 -0700

    Add generated code for recurrences chained

---
+++ LoopGen/recurrence-chained-20220730/4_stmts_recurrence_3_order_1_chain_length_09_atoms/4_stmts_recurrence_3_order_1_chain_length_09_atoms_de/core.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <string.h>
+
+extern int n_arrs;
+extern int size;
+
+int core(double (*restrict A)[2097152], double (*restrict B)[2097152], double (*restrict C)[2097152], double (*restrict D)[2097152]) {
+
+  for (int i = 3; i <= (size - 1) / n_arrs; i += 1) {
+    (*A)[i] = ((0 * (*A)[i - 1] + 0 * (*A)[i - 2]) + (*A)[i - 3]) + 0;
+    (*D)[i] = (0 * (*D)[i - 1] + 1 * (*D)[i - 2]) + (*D)[i - 3];
+    (*B)[i] = ((1 * (*B)[i - 1] + 2 * (*B)[i - 2]) + (*B)[i - 3]) + 0;
+    (*C)[i] = ((2 * (*C)[i - 1] + 2 * (*C)[i - 2]) + (*C)[i - 3]) + 0;
+  }
+  return 0;
+}
---
+++ LoopGen/recurrence-chained-20220730/4_stmts_recurrence_3_order_4_chain_length_21_atoms/4_stmts_recurrence_3_order_4_chain_length_21_atoms_de/core.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <string.h>
+
+extern int n_arrs;
+extern int size;
+
+int core(double (*restrict A)[2097152], double (*restrict B)[2097152], double (*restrict C)[2097152], double (*restrict D)[2097152]) {
+
+  for (int i = 3; i <= (size - 1) / n_arrs; i += 1) {
+    (*B)[i] = ((2 * (*B)[i - 1] + 2 * (*B)[i - 2]) + (*B)[i - 3]) + ((2 * (*C)[i - 1] + 1 * (*C)[i - 2]) + (*C)[i - 3]);
+    (*A)[i] = ((2 * (*A)[i - 1] + 1 * (*A)[i - 2]) + (*A)[i - 3]) + ((2 * (*B)[i - 1] + 1 * (*B)[i - 2]) + (*B)[i - 3]);
+    (*C)[i] = ((2 * (*C)[i - 1] + 1 * (*C)[i - 2]) + (*C)[i - 3]) + ((2 * (*D)[i - 1] + 2 * (*D)[i - 2]) + (*D)[i - 3]);
+    (*D)[i] = (2 * (*D)[i - 1] + 2 * (*D)[i - 2]) + (*D)[i - 3];
+  }
+  return 0;
+}
