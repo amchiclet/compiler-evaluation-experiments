1f8288da9f0d389268f630cd5d82750476091141
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2023-01-08 17:13:50 -0800

    Clean up 0s and 1s for recurrence fullness 20220814

--- code-generation-scripts/recurrence-fullness-20220814.py
+++ code-generation-scripts/recurrence-fullness-20220814.py
@@ -1,7 +1,7 @@
 from api import Skeleton, Mapping, CodegenConfig
 from random import randint, seed
-from codelet_generator import codelet_dir, generate_codelet_files, generate_batch_summary, SourceInfo
-from pattern_ast import get_ordered_assignments, get_accesses, count_ops
+from codelet_generator import codelet_dir, generate_codelet_files, generate_batch_summary_flex, SourceInfoFlex
+from pattern_ast import get_ordered_assignments, get_accesses, count_ops, simplify_0s_and_1s
 from itertools import product
 
 skeleton_code = """
@@ -52,6 +52,18 @@ seed(0)
 application = 'LoopGen'
 batch = 'recurrence-fullness-20220814'
 
+source_info_header = [
+    'name',
+    '# statements',
+    'order',
+    'group size',
+    'fullness',
+    '# arrays',
+    '# rhs array refs',
+    '# rhs uniq array refs',
+    '# rhs ops (excluding indices)',
+]
+
 programs = set()
 source_infos = []
 n_arrs = 3
@@ -88,42 +100,38 @@ for order in range(1, 4):
                                                        10,
                                                        [32000, n_arrs])
             config.array_as_ptr = True
+            simplify_0s_and_1s(skeleton.program)
             skeleton.generate_code(config)
 
             print(skeleton)
-        
+
             # Gather source information
-            si = SourceInfo()
-            si.name = code
-            si.n_stmts = 3
-            uniq_atoms = set()
+            si = SourceInfoFlex(source_info_header)
+            si['name'] = code
+            si['# statements'] = 3
+            si['# arrays'] = 3
+            si['order'] = order
+
+            array_refs = []
+            n_ops = 0
             graph = {}
             for assignment in get_ordered_assignments(skeleton.program):
-                n_arrays = 0
-                n_scalars = 0
-
-                for access in get_accesses(assignment, ignore_indices=False):
-                    if access.is_scalar():
-                        n_scalars += 1
-                    else:
-                        n_arrays += 1
-
-                # extra RHS counting
                 lhs = assignment.lhs.var
+                n_ops += count_ops(assignment.rhs, ignore_indices=True)
                 for access in get_accesses(assignment.rhs, ignore_indices=True):
-                    # count unique aroms
-                    uniq_atoms.add(str(access))
+                    # count array refs
+                    if not access.is_scalar():
+                        array_refs.append(str(access))
+
                     # count groups
                     if lhs not in graph:
                         graph[lhs] = set()
                     if access.var not in graph:
                         graph[access.var] = set()
                     graph[lhs].add(access.var)
-                    graph[access.var].add(lhs)
-            
-                si.scalars.append(n_scalars)
-                si.arrays.append(n_arrays)
-                si.ops.append(count_ops(assignment))
+                    graph[access.var].add(lhs)            
+
+            assert(len(array_refs) == n_atoms)
 
             def dfs(v, visited, acc):
                 if v in visited:
@@ -148,13 +156,14 @@ for order in range(1, 4):
                 if len(g) > max_group_size:
                     max_group_size = len(g)
 
+            si['group size'] = max_group_size
+            si['# rhs array refs'] = n_atoms
+            si['# rhs uniq array refs'] = len(set(array_refs))
+            si['# rhs ops (excluding indices)'] = n_ops
             fullness = n_atoms/3
-            si.extra_columns.append('recurrence')
-            si.extra_columns.append(order)
-            si.extra_columns.append(n_atoms) # number of atoms
-            si.extra_columns.append(len(uniq_atoms)) # number of unique atoms
-            si.extra_columns.append(max_group_size)
-            si.extra_columns.append(f'{fullness:.2f}')
+            si['fullness'] = f'{fullness:.2f}'
             source_infos.append(si)
+            print(si.header())
+            print(si.columns())
 
-generate_batch_summary(application, batch, source_infos, ['computation', 'order', '# atoms', '# uniq atoms', 'group size', 'fullness'])
+generate_batch_summary_flex(application, batch, source_infos, source_info_header)
