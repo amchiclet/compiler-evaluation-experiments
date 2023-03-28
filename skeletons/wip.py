73554357123f7576de380158f2603fcc056473e5
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2023-03-27 22:46:43 -0700

    [WIP] wip.py can generate some recurrence code

--- skeletons/wip.py
+++ skeletons/wip.py
@@ -42,10 +42,10 @@ def fill_empty_statements(n_empty_stmts, family_name, skeleton):
 
 def stmt_skeleton(n_rhs_exprs, ith_stmt):
     def term(ith_stmt):
-        return f'#_:{rhs_coef(ith_stmt)}# * #_:{rhs_arr(ith_stmt)}#'
+        return f'#_:coef# * #_:{rhs_arr(ith_stmt)}#'
 
     rhs_expr = ' + '.join([term(ith_stmt) for _ in range(n_rhs_exprs)])
-    lhs_expr = lhs_arr(ith_stmt)
+    lhs_expr = f'#_:{lhs_arr(ith_stmt)}#'
     return f'{lhs_expr} = {rhs_expr};'
 
 def fill_linear_statements(n_stmts, n_total_zeroes, skeleton):
@@ -66,14 +66,37 @@ def fill_linear_statements(n_stmts, n_total_zeroes, skeleton):
     stmts = [Mapping('_', stmts, is_finite=True)]
     return skeleton.fill_statements(stmts)
 
-def partial_match(node_family, mapping_family):
-    return mapping_family in node_family
-
 def fill_coefs(n_coefs, skeleton):
     coefs = [f'{uniform(1.1, 9.9):.1f}' for _ in range(n_coefs)]
-
     exprs = [Mapping('coef', coefs, is_finite=True)]
-    return skeleton.fill_expressions(exprs, matching_function=partial_match)
+    return skeleton.fill_expressions(exprs)
+
+def fill_lhs(n_stmts, skeleton):
+    exprs = [
+        Mapping(lhs_arr(i), [f'Arr{i+1:02d}'])
+        for i in range(n_stmts)
+    ]
+    return skeleton.fill_expressions(exprs)
+
+def fill_rhs_loop(loop_size, skeleton):
+    exprs = [
+        Mapping(rhs_arr(i), [f'Arr{((i+1)%loop_size)+1:02d}[#_:index#]'], is_finite=True)
+        for i in range(loop_size)
+    ]
+    return skeleton.fill_expressions(exprs)
+
+def fill_rhs(loop_size, skeleton):
+    exprs = [
+        Mapping(rhs_arr(i), [f'Arr{i+1:02d}[#_:index#]'])
+        for i in range(loop_size)
+    ]
+    return skeleton.fill_expressions(exprs)
+
+def fill_index(skeleton):
+    exprs = [
+        Mapping('index', ['i-1', 'i-2', 'i-3'])
+    ]
+    return skeleton.fill_expressions(exprs)
 
 def gen_program():
     n_stmts = randint(1, max_stmts)
@@ -93,6 +116,18 @@ def gen_program():
 
     n_coefs = max_total_exprs - n_total_zeroes
     skeleton = fill_coefs(n_coefs, skeleton)
+    print(skeleton)
+
+    skeleton = fill_lhs(n_stmts, skeleton)
+    print(skeleton)
+
+    skeleton = fill_rhs_loop(n_stmts, skeleton)
+    print(skeleton)
+
+    skeleton = fill_rhs(n_stmts, skeleton)
+    print(skeleton)
+
+    skeleton = fill_index(skeleton)
     print(skeleton)
 
 gen_program()
