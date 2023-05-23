048356f7d40ecbdd56ff3475e6d114e4228d68b7
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2023-05-22 21:57:17 -0500

    Add logic to search near previously hit targets

--- generate-code.py
+++ generate-code.py
@@ -12,6 +12,7 @@ import random
 import pandas as pd
 import os
 import stat
+import loguru
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--output_dir', type=pathlib.Path, default=pathlib.Path('./out'))
@@ -31,20 +32,24 @@ if args.random_state is not None:
 
 checksums = checksum.gather_checksums(args.output_dir, '*/*/*/core.c')
 
+logger = loguru.logger
+
 experiment_name = args.experiment_name
 if args.experiment_name is None:
     t = datetime.datetime.now()
     experiment_name = f'experiment-{t.year}{t.month:02d}{t.day:02d}-{t.hour:02d}{t.minute:02d}'
 
-def generate_batch(batch, n_codelets, max_try_factor=3):
+def generate_batch(batch, n_codelets, max_try_factor=3, params_prototype=None):
     max_tries = n_codelets * max_try_factor
     sis = []
+    params = {}
     for _ in range(max_tries):
         index = len(sis) + 1
-        result = generate_codelet(batch, index)
+        result = generate_codelet(batch, index, params_prototype)
         if result is not None:
-            code, codelet, si = result
-            print(f'Generated {batch}/{code}/codelet')
+            code, codelet, si, codegen_params = result
+            params[code] = codegen_params
+            logger.debug(f'Generated {batch}/{code}/{codelet}')
             sis.append(si)
             if len(sis) == n_codelets:
                 break
@@ -53,9 +58,18 @@ def generate_batch(batch, n_codelets, max_try_factor=3):
     codelet_generator.generate_batch_summary_flex(
         args.output_dir, batch, sis, generate.source_info_header
     )
-    return sis
+    return sis, params
 
-def generate_codelet(batch, index):
+def create_mutations(attributes):
+    decrement = lambda x: x-1
+    stay = lambda x: x
+    increment = lambda x: x+1
+
+    mutations = {attr:stay for attr in attributes}
+    mutations[random.choice(attributes)] = random.choice([increment, stay, decrement])
+    return mutations
+
+def generate_codelet(batch, index, params_prototype=None):
     t = datetime.datetime.now()
     code = f'program_{index:02d}'
     codelet = f'{code}_de'
@@ -65,20 +79,23 @@ def generate_codelet(batch, index):
     randomstate_path = codelet_path / 'random.state'
     randomstate.checkpoint(randomstate_path)
 
-    n_stmts = random.randint(3, 5)
-    n_ops = random.randint(n_stmts, n_stmts*5)
-    n_lc_deps = random.randint(0, n_stmts)
-    n_li_deps = random.randint(0, n_stmts)
-    n_ro = random.randint(1, 5)
-    n_arrs = random.randint(n_stmts+1, n_stmts+n_ro)
-    gen_result = generate.gen_program(n_stmts, n_arrs, n_ops, n_lc_deps, n_li_deps, n_ro)
+    if params_prototype is None:
+        codegen_params = generate.randomly_create_code_generation_parameters()
+    else:
+        codegen_params = params_prototype.mutate(create_mutations)
+
+    gen_result = generate.gen_program(codegen_params)
     if gen_result is None:
         return None
 
     skeleton, si, config, default_inputs = gen_result
-    si['name'] = code
     if skeleton is None:
         return None
+
+    si['batch'] = batch
+    si['name'] = code
+    si['codelet name'] = codelet
+    si['generate function'] = 'skeletons.generate.gen_program'
 
     config.template_dir = 'codelet-template-int-inputs'
     config.output_dir = codelet_generator.generate_codelet_files(
@@ -88,18 +105,18 @@ def generate_codelet(batch, index):
     skeleton.generate_code(config)
     new_checksum = checksum.get_checksum(codelet_path / 'core.c')
     if new_checksum in checksums:
-        print('codelet already generated')
+        logger.debug('codelet already generated')
         return None
 
-    return code, codelet, si
+    return code, codelet, si, codegen_params
 
 def extract_loop(batch_path):
-    names = []
-    codes = []
+    loops = {}
 
     for path in sorted(pathlib.Path(batch_path).glob('*/*/core.c')):
-        codelet = path.parts[-3]
-        names.append(f'LoopGen: {codelet}_de')
+        name = path.parts[-3]
+
+        # names.append(f'LoopGen: {codelet}_de')
         lines = []
         with open(path) as f:
             function_start = False
@@ -116,12 +133,15 @@ def extract_loop(batch_path):
         for line in lines:
             if len(line.strip()) > 0:
                 no_empty.append(line)
-        codes.append(''.join(no_empty))
+        # codes.append(''.join(no_empty))
 
-    df = pd.DataFrame()
-    df['name'] = names
-    df['code'] = codes
-    return df
+        code = ''.join(no_empty)
+        loops[name] = code
+    # df = pd.DataFrame()
+    # df['name'] = names
+    # df['code'] = codes
+    # return df
+    return loops
 
 # Overview
 # 1) generate batch
@@ -143,13 +163,22 @@ full_df = pd.DataFrame()
 result_df = pd.DataFrame()
 
 count = 0
+
+# Once we find codegen params that work, this variable
+# will be used, so subsequent code generation can be based
+# on it.
+params_prototype = None
+
 for attempt in range(1, args.max_attempts+1):
     # generate batch
     batch_prefix = experiment_name
     if args.batch_prefix is not None:
         batch_prefix = args.batch_prefix
     batch = f'{batch_prefix}-batch{attempt:02d}'
-    source_infos = generate_batch(batch, args.batch_size)
+    source_infos, codegen_params = generate_batch(batch,
+                                                  args.batch_size,
+                                                  max_try_factor=3,
+                                                  params_prototype=params_prototype)
 
     # run batch
 
@@ -158,18 +187,18 @@ for attempt in range(1, args.max_attempts+1):
     log_path = current_dir / f'{batch}.log'
     vrun_dir = current_dir / '..' / 'cape-experiment-scripts' / 'vrun'
     vrun_input = f'{current_dir.resolve().name}/{args.output_dir}/{batch}'
-    print(vrun_input)
-    command = delegator.run(f'./build_vrun_script {vrun_input}', cwd=vrun_dir)
-    print(command.out)
-    print(command.err)
+    logger.debug(vrun_input)
+    command = delegator.run(f'./build_vrun_script {vrun_input} vrun_new.sh', cwd=vrun_dir)
+    logger.debug(command.out)
+    logger.debug(command.err)
 
     # run the newly generated vrun_new.sh
     vrun_new_sh = vrun_dir / 'vrun_new.sh'
     st = os.stat(vrun_new_sh)
     os.chmod(vrun_new_sh, st.st_mode | stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH)
 
-    print(f'Running cape script. To view ')
-    print(f'less +F {log_path.resolve()}')
+    logger.info(f'Running cape script. To view ')
+    logger.info(f'less +F {log_path.resolve()}')
     command = delegator.run(f'./vrun_new.sh test | tee {log_path.resolve()}', cwd=vrun_dir)
 
     # find the generated SI data csv file
@@ -182,49 +211,54 @@ for attempt in range(1, args.max_attempts+1):
     si_df = pd.read_csv(si_path)
 
     # add source info to the dataframe
+
+    loops = extract_loop(args.output_dir / batch)
+
     source_info_dict = {}
+    source_info_dict['si name'] = []
+    source_info_dict['code'] = []
     for source_info in source_infos:
         for k, v in source_info.m.items():
             if k not in source_info_dict:
                 source_info_dict[k] = []
-            if k == 'name':
-                source_info_dict[k].append(f'LoopGen: {v}_de')
-            else:
-                source_info_dict[k].append(v)
-    source_info_df = pd.DataFrame.from_dict(source_info_dict)
-    print(source_info_df)
-    si_df = pd.merge(si_df, source_info_df, how="outer", left_on="Name", right_on="name")
+            source_info_dict[k].append(v)
+        source_info_dict['si name'].append(f'LoopGen: {source_info["name"]}_de')
+        source_info_dict['code'].append(loops[source_info['name']])
 
-    # get the loop code
-    loops_df = extract_loop(args.output_dir / batch)
-    print(loops_df)
-    si_df = pd.merge(si_df, loops_df, how="outer", left_on="Name", right_on="name")
+    source_info_df = pd.DataFrame.from_dict(source_info_dict)
+    logger.debug(source_info_df)
+    si_df = pd.merge(si_df, source_info_df, how="outer", left_on="Name", right_on="si name")
+
+    # si_df = pd.merge(si_df, loops_df, how="outer", left_on="Name", right_on="name")
 
     full_df = full_df.append(si_df)
 
     yield_df = si_df.loc[si_df['Tier'].isin(args.tiers) & si_df['Sat_Node'].isin(control_unit_nodes)]
     result_df = result_df.append(yield_df)
     if not yield_df.empty:
-        print(f'Found codelets saturating the control unit at tier {args.tiers}')
-        print(yield_df[['Name', 'Tier', 'Sat_Node']])
+        logger.debug(f'Found codelets saturating the control unit at tier {args.tiers}')
+        logger.debug(yield_df[['Name', 'si name', 'name', 'Tier', 'Sat_Node']])
         count += len(yield_df)
         if count >= args.how_many:
             break
+        # Just use the first yield we got
+        yield_name = yield_df['name'].values[0]
+        params_prototype = codegen_params[yield_name]
         continue
 
-    print(f'Finished attempt {attempt}/{args.max_attempts}')
-    print(f'Did not find any codelets saturating the control units at tier {args.tiers}')
-    print(full_df[['Name', 'Tier', 'Sat_Node']])
+    logger.debug(f'Finished attempt {attempt}/{args.max_attempts}')
+    logger.debug(f'Did not find any codelets saturating the control units at tier {args.tiers}')
+    logger.debug(full_df[['Name', 'Tier', 'Sat_Node']])
 
 columns = ['Name', 'Tier', 'Sat_Node', '# statements', 'code']
 full_df = full_df.reindex()
-result_df = full_df.reindex()
+result_df = result_df.reindex()
 
-print('*********** All codelets generated **********')
-print(full_df[columns])
+logger.info('*********** All codelets generated **********')
+logger.info(full_df[columns])
 
-print(f'************ Codelets saturating the control unit at tiers {args.tiers} ***********')
-print(result_df[columns])
+logger.info(f'************ Codelets saturating the control unit at tiers {args.tiers} ***********')
+logger.info(result_df[columns])
 
 full_df.to_csv(f'{args.output_dir}/{experiment_name}_codelet_all.csv', index = True, header = True)
 result_df.to_csv(f'{args.output_dir}/{experiment_name}_codelet_yield.csv', index = True, header = True)
--- skeletons/generate.py
+++ skeletons/generate.py
@@ -3,6 +3,7 @@ from random import randint, seed, uniform, choices
 from codelet_generator import codelet_dir, generate_codelet_files, generate_batch_summary_flex, SourceInfoFlex
 from pattern_ast import get_ordered_assignments, get_accesses, count_ops, simplify_0s_and_1s
 from itertools import product
+from loguru import logger
 
 skeleton_code = """
 declare size;
@@ -47,6 +48,66 @@ def fill_lhs(lhs_arrs, skeleton):
         exprs.append(Mapping(f'stmt{i}_lhs', [f'{lhs_arr}[i]']))
     return skeleton.fill_expressions(exprs)
 
+class CodeGenerationParameters:
+    def __init__(self, n_stmts, n_arrs, n_ops, n_lc_deps, n_li_deps, n_ro):
+        self.n_stmts = n_stmts
+        self.n_arrs = n_arrs
+        self.n_ops = n_ops
+        self.n_lc_deps = n_lc_deps
+        self.n_li_deps = n_li_deps
+        self.n_ro = n_ro
+    def __str__(self):
+        return (f'# of statements       = {n_stmts}\n'
+                f'# of arrays           = {n_arrs}\n'
+                f'# of ops              = {n_ops}\n'
+                f'loop-carried deps     = {n_lc_deps}\n'
+                f'loop-independent deps = {n_li_deps}\n'
+                f'read-only accesses    = {n_ro}\n'
+        )
+    def extract(self):
+        return (self.n_stmts,
+                self.n_arrs,
+                self.n_ops,
+                self.n_lc_deps,
+                self.n_li_deps,
+                self.n_ro
+        )
+    def mutate(self, create_mutations):
+        logger.debug('mutating')
+        while True:
+            mutations = create_mutations(['n_stmts', 'n_arrs', 'n_ops', 'n_lc_deps', 'n_li_deps', 'n_ro'])
+            n_stmts = mutations['n_stmts'](self.n_stmts)
+            n_arrs = mutations['n_arrs'](self.n_arrs)
+            n_ops = mutations['n_ops'](self.n_ops)
+            n_lc_deps = mutations['n_lc_deps'](self.n_lc_deps)
+            n_li_deps = mutations['n_li_deps'](self.n_li_deps)
+            n_ro = mutations['n_ro'](self.n_ro)
+
+            if n_stmts < 3 or n_stmts > 5:
+                continue
+            if n_ops < n_stmts or n_ops > n_stmts * 5:
+                continue
+            if n_lc_deps < 0 or n_lc_deps > n_stmts:
+                continue
+            if n_li_deps < 0 or n_li_deps > n_stmts:
+                continue
+            if n_ro < 1 or n_ro > 5:
+                continue
+            if n_arrs < n_stmts+1 or n_arrs > n_stmts + n_ro:
+                continue
+
+            return CodeGenerationParameters(n_stmts, n_arrs, n_ops, n_lc_deps, n_li_deps, n_ro)
+
+def randomly_create_code_generation_parameters():
+    n_stmts = randint(3, 5)
+    n_ops = randint(n_stmts, n_stmts*5)
+    n_lc_deps = randint(0, n_stmts)
+    n_li_deps = randint(0, n_stmts)
+    n_ro = randint(1, 5)
+    n_arrs = randint(n_stmts+1, n_stmts+n_ro)
+    params = CodeGenerationParameters(n_stmts, n_arrs, n_ops, n_lc_deps, n_li_deps, n_ro)
+    return params
+
 # TODO: parameters should include
 # 1) number of arrays
 # 2) number of ALU instructions
@@ -55,18 +116,19 @@ def fill_lhs(lhs_arrs, skeleton):
 # 5) indexing type: stride=1, regular, indirect
 # 6) cycles: self, cross-statement
 # 7) datasize
-def gen_program(n_stmts, n_arrs, n_ops, n_lc_deps, n_li_deps, n_ro):
-    print(f'n_stmts({n_stmts}) '
-          f'n_arrs({n_arrs}) '
-          f'n_ops({n_ops}) '
-          f'n_lc_deps({n_lc_deps}) '
-          f'n_li_deps({n_li_deps}) '
-          f'n_ro({n_ro})')
-    print('---- Skeleton ----')
+def gen_program(parameters):
+    n_stmts, n_arrs, n_ops, n_lc_deps, n_li_deps, n_ro = parameters.extract()
+    logger.debug(f'n_stmts({n_stmts}) '
+                 f'n_arrs({n_arrs}) '
+                 f'n_ops({n_ops}) '
+                 f'n_lc_deps({n_lc_deps}) '
+                 f'n_li_deps({n_li_deps}) '
+                 f'n_ro({n_ro})')
+    logger.debug('---- Skeleton ----')
     skeleton = Skeleton(skeleton_code)
     n_empty_stmts = max_stmts - n_stmts
     if n_empty_stmts < 0:
-        print("n_empty_stmts < 0")
+        logger.debug("n_empty_stmts < 0")
         return None
     skeleton = fill_empty_statements(n_empty_stmts, skeleton)
 
@@ -82,24 +144,23 @@ def gen_program(n_stmts, n_arrs, n_ops, n_lc_deps, n_li_deps, n_ro):
 
     # distribute ops per statement
     n_ops_dist = [len(ops) for ops in distribute(range(n_ops))]
-    print('---- Fill statements ----')
+    logger.debug('---- Fill statements ----')
     skeleton = fill_statements(n_ops_dist, skeleton)
-    print(skeleton)
-    print()
+    logger.debug(skeleton)
 
     # fill arrays
     arrs = [f'Arr{i:02d}' for i in range(n_arrs)]
 
     # LHS
-    print('---- Fill LHS ----')
+    logger.debug('---- Fill LHS ----')
 
     lhs_arrs = arrs[:n_stmts]
     skeleton = fill_lhs(lhs_arrs, skeleton)
-    print(skeleton)
+    logger.debug(skeleton)
 
     n_exprs = sum([n_ops+1 for n_ops in n_ops_dist])
     if n_exprs < n_lc_deps + n_lc_deps + n_ro:
-        print("n_exprs < n_lc_deps + n_lc_deps + n_ro:")
+        logger.debug("n_exprs < n_lc_deps + n_lc_deps + n_ro:")
         return None
 
     rhs_pool = []
@@ -124,26 +185,19 @@ def gen_program(n_stmts, n_arrs, n_ops, n_lc_deps, n_li_deps, n_ro):
     def partial_match(node_family, mapping_family):
         return mapping_family in node_family
 
-    print('---- Fill RHS ----')
+    logger.debug('---- Fill RHS ----')
     skeleton = skeleton.fill_expressions(
         [ Mapping('rhs', rhs_pool, is_finite=True), ],
         matching_function=partial_match
     )
-    print(skeleton)
-    print()
+    logger.debug(skeleton)
 
     # fill operations
-    print('---- Fill operations ----')
+    logger.debug('---- Fill operations ----')
     skeleton = skeleton.fill_operations([Mapping('_', ['*', '+'])])
-    print(skeleton)
+    logger.debug(skeleton)
 
-    print(f'# of statements       = {n_stmts}\n'
-          f'# of arrays           = {n_arrs}\n'
-          f'# of ops              = {n_ops}\n'
-          f'loop-carried deps     = {n_lc_deps}\n'
-          f'loop-independent deps = {n_li_deps}\n'
-          f'read-only accesses    = {n_ro}\n'
-    )
+    # print debug string
 
     config = CodegenConfig()
     config.possible_values = {
@@ -161,11 +215,14 @@ def gen_program(n_stmts, n_arrs, n_ops, n_lc_deps, n_li_deps, n_ro):
     }
     config.array_as_ptr = True
 
-    print(skeleton)
+    logger.debug(skeleton)
 
     # Gather source information
     si = SourceInfoFlex(source_info_header)
-    si['name'] = None
+    # si['batch'] = None
+    # si['name'] = None
+    # si['codelet name'] = None
+    # si['generate function'] = None
     si['# statements'] = n_stmts
     si['# arrays'] = n_arrs
 
@@ -185,8 +242,6 @@ def gen_program(n_stmts, n_arrs, n_ops, n_lc_deps, n_li_deps, n_ro):
     si['# loop-carried deps'] = n_lc_deps
     si['# loop-independent deps'] = n_li_deps
     si['# read-only references'] = n_ro
-    print(si.header())
-    print(si.columns())
 
     # for this codelet, the default size is 32000
     # n_arrs will be whatever was given
@@ -196,6 +251,10 @@ def gen_program(n_stmts, n_arrs, n_ops, n_lc_deps, n_li_deps, n_ro):
 
 source_info_header = [
     'name',
+    'batch',
+    'codelet name',
+    'generate function',
+    'si name',
     '# statements',
     '# arrays',
     '# rhs array refs',
