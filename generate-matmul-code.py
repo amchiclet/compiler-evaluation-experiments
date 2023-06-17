b7ab672dcd0f251455f25a00ff3184dddca923c4
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2023-06-17 16:01:12 -0500

    End to end matmul code generation

--- generate-matmul-code.py
+++ generate-matmul-code.py
@@ -59,15 +59,6 @@ def generate_batch(batch, n_codelets, max_try_factor=3, params_prototype=None):
     )
     return sis, params
 
-def create_mutations(attributes):
-    decrement = lambda x: x-1
-    stay = lambda x: x
-    increment = lambda x: x+1
-
-    mutations = {attr:stay for attr in attributes}
-    mutations[random.choice(attributes)] = random.choice([increment, stay, decrement])
-    return mutations
-
 def generate_codelet(batch, index, params_prototype=None):
     t = datetime.datetime.now()
     code = f'program_{index:02d}'
@@ -91,7 +82,7 @@ def generate_codelet(batch, index, params_prototype=None):
     si['batch'] = batch
     si['name'] = code
     si['codelet name'] = codelet
-    si['generate function'] = 'skeletons.generate.gen_program'
+    si['generate function'] = 'skeletons.matmul.gen_program'
 
     config.template_dir = 'codelet-template-int-inputs'
     config.output_dir = codelet_generator.generate_codelet_files(
@@ -151,104 +142,72 @@ control_unit_nodes = [
 ]
 
 # save all data in full_df
-# save data that meets constraints in result_df
 full_df = pd.DataFrame()
-result_df = pd.DataFrame()
 
-count = 0
-print('hello world')
-for attempt in range(1, args.max_attempts+1):
-    # generate batch
-    batch_prefix = experiment_name
-    if args.batch_prefix is not None:
-        batch_prefix = args.batch_prefix
-    batch = f'{batch_prefix}-batch{attempt:02d}'
-    source_infos, codegen_params = generate_batch(batch,
-                                                  args.batch_size,
-                                                  max_try_factor=3)
+# generate batch
+batch_prefix = experiment_name
+if args.batch_prefix is not None:
+    batch_prefix = args.batch_prefix
+batch = f'{batch_prefix}-batch'
+source_infos, codegen_params = generate_batch(batch,
+                                              args.batch_size,
+                                              max_try_factor=3)
 
-    exit()
+# run 'build_vrun_script batch' to generate vrun_new.sh
+current_dir = pathlib.Path('.')
+log_path = current_dir / f'{batch}.log'
+vrun_dir = current_dir / '..' / 'cape-experiment-scripts' / 'vrun'
+vrun_input = f'{current_dir.resolve().name}/{args.output_dir}/{batch}'
+logger.debug(vrun_input)
+vrun_sh = 'vrun_matmul_new.sh'
+command = delegator.run(f'./build_vrun_matmul {vrun_input} matmul_sizes.txt {vrun_sh}', cwd=vrun_dir)
+logger.debug(command.out)
+logger.debug(command.err)
 
-    # TODO: pass in 3 parameters
-    # run batch
+# run the newly generated vrun script
+vrun_new_sh = vrun_dir / vrun_sh
+st = os.stat(vrun_new_sh)
+os.chmod(vrun_new_sh, st.st_mode | stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH)
 
-    # run 'build_vrun_script batch' to generate vrun_new.sh
-    current_dir = pathlib.Path('.')
-    log_path = current_dir / f'{batch}.log'
-    vrun_dir = current_dir / '..' / 'cape-experiment-scripts' / 'vrun'
-    vrun_input = f'{current_dir.resolve().name}/{args.output_dir}/{batch}'
-    logger.debug(vrun_input)
-    command = delegator.run(f'./build_vrun_script {vrun_input} vrun_new.sh', cwd=vrun_dir)
-    logger.debug(command.out)
-    logger.debug(command.err)
+logger.info(f'Running cape script. To view ')
+logger.info(f'less +F {log_path.resolve()}')
+command = delegator.run(f'./{vrun_sh} test | tee {log_path.resolve()}', cwd=vrun_dir)
 
-    # run the newly generated vrun_new.sh
-    vrun_new_sh = vrun_dir / 'vrun_new.sh'
-    st = os.stat(vrun_new_sh)
-    os.chmod(vrun_new_sh, st.st_mode | stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH)
+# find the generated SI data csv file
+line_start = command.out.index("Cape SI data")
+si_filename_start = command.out.index(":", line_start) + 2
+si_filename_end = command.out.index(".csv", line_start) + 4
+si_path = command.out[si_filename_start:si_filename_end].strip()
+si_csv = pathlib.Path(si_path)
 
-    logger.info(f'Running cape script. To view ')
-    logger.info(f'less +F {log_path.resolve()}')
-    command = delegator.run(f'./vrun_new.sh test | tee {log_path.resolve()}', cwd=vrun_dir)
+si_df = pd.read_csv(si_path)
 
-    # find the generated SI data csv file
-    line_start = command.out.index("Cape SI data")
-    si_filename_start = command.out.index(":", line_start) + 2
-    si_filename_end = command.out.index(".csv", line_start) + 4
-    si_path = command.out[si_filename_start:si_filename_end].strip()
-    si_csv = pathlib.Path(si_path)
+# add source info to the dataframe
 
-    si_df = pd.read_csv(si_path)
+loops = extract_loop(args.output_dir / batch)
 
-    # add source info to the dataframe
+source_info_dict = {}
+source_info_dict['si name'] = []
+source_info_dict['code'] = []
 
-    loops = extract_loop(args.output_dir / batch)
+for source_info in source_infos:
+    for k, v in source_info.m.items():
+        if k not in source_info_dict:
+            source_info_dict[k] = []
+        source_info_dict[k].append(v)
+    source_info_dict['si name'].append(f'LoopGen: {source_info["name"]}_de')
+    source_info_dict['code'].append(loops[source_info['name']])
 
-    source_info_dict = {}
-    source_info_dict['si name'] = []
-    source_info_dict['code'] = []
-    for source_info in source_infos:
-        for k, v in source_info.m.items():
-            if k not in source_info_dict:
-                source_info_dict[k] = []
-            source_info_dict[k].append(v)
-        source_info_dict['si name'].append(f'LoopGen: {source_info["name"]}_de')
-        source_info_dict['code'].append(loops[source_info['name']])
+source_info_df = pd.DataFrame.from_dict(source_info_dict)
+logger.debug(source_info_df)
+si_df = pd.merge(si_df, source_info_df, how="outer", left_on="Name", right_on="si name")
 
-    source_info_df = pd.DataFrame.from_dict(source_info_dict)
-    logger.debug(source_info_df)
-    si_df = pd.merge(si_df, source_info_df, how="outer", left_on="Name", right_on="si name")
-
-    # si_df = pd.merge(si_df, loops_df, how="outer", left_on="Name", right_on="name")
-
-    full_df = full_df.append(si_df)
-
-    yield_df = si_df.loc[si_df['Tier'].isin(args.tiers) & si_df['Sat_Node'].isin(control_unit_nodes)]
-    result_df = result_df.append(yield_df)
-    if not yield_df.empty:
-        logger.debug(f'Found codelets saturating the control unit at tier {args.tiers}')
-        logger.debug(yield_df[['Name', 'si name', 'name', 'Tier', 'Sat_Node']])
-        count += len(yield_df)
-        if count >= args.how_many:
-            break
-        # Just use the first yield we got
-        yield_name = yield_df['name'].values[0]
-        params_prototype = codegen_params[yield_name]
-        continue
-
-    logger.debug(f'Finished attempt {attempt}/{args.max_attempts}')
-    logger.debug(f'Did not find any codelets saturating the control units at tier {args.tiers}')
-    logger.debug(full_df[['Name', 'Tier', 'Sat_Node']])
+full_df = full_df.append(si_df)
 
 columns = ['Name', 'Tier', 'Sat_Node', '# statements', 'code']
 full_df = full_df.reindex()
-result_df = result_df.reindex()
 
 logger.info('*********** All codelets generated **********')
-logger.info(full_df[columns])
-
-logger.info(f'************ Codelets saturating the control unit at tiers {args.tiers} ***********')
-logger.info(result_df[columns])
+logger.info(f'\n{full_df[columns]}')
 
 full_df.to_csv(f'{args.output_dir}/{experiment_name}_codelet_all.csv', index = True, header = True)
-result_df.to_csv(f'{args.output_dir}/{experiment_name}_codelet_yield.csv', index = True, header = True)
